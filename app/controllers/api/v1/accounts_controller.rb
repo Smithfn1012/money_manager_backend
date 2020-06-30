5b6e201c5ba8dbc7bf61d98524481bb39a9a1ceb
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-30 14:22:03 -0500

    def create accounts controller

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -4,4 +4,13 @@ class Api::V1::AccountsController < ApplicationController
         @accounts = Account.all
         render json: @accounts
     end
+
+    def create
+        @account = Account.new(account_params)
+        if @account.save
+            render json: @account
+        else
+            render json: {error: 'Error creating account'}
+        end
+    end
 end
