c2602b06f63f300083b74d315bf2083c605dcb0a
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-30 14:19:55 -0500

    add balance_update to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,19 @@
 class Account < ApplicationRecord
     has_many :transactions
     validates :name, :balance, presence: true
+
+    def balance_update(transaction)
+        if transaction.exchange == 'deposit'
+            self.balance = self.balance + transaction.amount
+            self.save
+        elsif transaction.exchange == 'withdraw'
+            if self.balance >= transaction.amount
+                self.balance = self.balance - transaction.amount
+                self.save
+            else
+                return 'Insufficient Funds.'
+            end
+        end
+    end
+
 end
