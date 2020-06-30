4afb4ec96ce89691e7b38539702811a43f3afbf8
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-30 14:27:58 -0500

    def show transaction controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -5,4 +5,10 @@ before_action :set_account
     @transactions = Transaction.all
     render json: @transactions
   end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+    render json: @transaction
+  end
+  
 end
