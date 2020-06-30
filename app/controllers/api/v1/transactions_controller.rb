a558563c1cfbcfd0627b3e43049e36243d85d219
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-30 14:27:10 -0500

    def index transaction controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::TransactionsController < ApplicationController
+before_action :set_account
+
+  def index
+    @transactions = Transaction.all
+    render json: @transactions
+  end
 end
